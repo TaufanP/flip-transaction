2d1f297bb370682585682daf2bbf2f7f1a93df80
Taufan Prihantoro <taufan@airasiaredtix.com>
2021-10-25 05:34:55 +0700

    dropdown

--- src/screens/TransactionDetail/index.tsx
+++ src/screens/TransactionDetail/index.tsx
@@ -3,13 +3,18 @@ import {
   RouteProp,
   useRoute,
 } from "@react-navigation/core";
-import React, { useRef } from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { LayoutChangeEvent, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { ArrowTailed } from "../../../assets";
 import { Button, Container, Gap, Header, TextItem } from "../../components";
 import { colors, spacing as sp } from "../../constants";
-import { currencyFormat } from "../../helper";
+import { currencyFormat, widthPercent } from "../../helper";
 import { dateFormater } from "../../helper/dateFormat";
 import { StackParamsList } from "../../types/screens";
 import styles from "./styles";
@@ -20,7 +25,11 @@ interface TransactionDetailProps {
 
 const TransactionDetail = ({ navigation }: TransactionDetailProps) => {
   const route = useRoute<RouteProp<StackParamsList, "TRANSACTION_DETAIL">>();
-  const isMounted = useRef<boolean>();
+
+  const [boxHeight, setBoxHeight] = useState<number>(0);
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
+
+  const heightValue = useSharedValue(0);
 
   const {
     beneficiary_bank,
@@ -35,6 +44,22 @@ const TransactionDetail = ({ navigation }: TransactionDetailProps) => {
     created_at,
   } = route.params;
 
+  const maskStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: heightValue.value }],
+  }));
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const { height } = event.nativeEvent.layout;
+    setBoxHeight(height + 24);
+    heightValue.value = withTiming(height + 24);
+  };
+
+  const toggle = () => setIsExpanded((current) => !current);
+
+  useEffect(() => {
+    heightValue.value = withTiming(isExpanded ? boxHeight : 0);
+  }, [isExpanded]);
+
   return (
     <Container>
       <Header label={"Rincian Transaksi"} onPress={() => navigation.goBack()} />
@@ -48,12 +73,14 @@ const TransactionDetail = ({ navigation }: TransactionDetailProps) => {
         <Gap vertical={sp.xxs} />
         <View style={[styles.box, styles.container, styles.space]}>
           <TextItem type="b.16.text1.u">detail transaksi </TextItem>
-          <Button>
-            <TextItem type="n.14.primary1.c">tutup</TextItem>
+          <Button onPress={toggle}>
+            <TextItem type="n.14.primary1.c">
+              {isExpanded ? `tutup` : "buka"}
+            </TextItem>
           </Button>
         </View>
         <Gap vertical={sp.xxs} />
-        <View style={styles.box}>
+        <View style={styles.box} onLayout={onLayout}>
           <View style={styles.rowCenter}>
             <TextItem type={"b.16.text1.u"}>{sender_bank}</TextItem>
             <ArrowTailed
@@ -90,6 +117,17 @@ const TransactionDetail = ({ navigation }: TransactionDetailProps) => {
           <Gap vertical={sp.sm} />
           <TextItem type="b.14.text1.u">waktu dibuat</TextItem>
           <TextItem type="n.14.text1">{dateFormater(created_at)}</TextItem>
+          <Animated.View
+            style={[
+              maskStyle,
+              {
+                width: widthPercent(100),
+                height: boxHeight,
+                backgroundColor: colors.white1,
+                position: "absolute",
+              },
+            ]}
+          />
         </View>
       </ScrollView>
     </Container>
